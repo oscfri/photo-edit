938834156d45aefac3ca8290f31cbd3d16210f44
oscfri <oscar.c.friberg@gmail.com>
2025-01-05 23:13:02 +0100

    Implemented powf cache

--- src/conversions.rs
+++ src/conversions.rs
@@ -6,115 +6,167 @@ use rayon::prelude::*;
  * Conversions based on: https://www.easyrgb.com/en/math.php
  */
 
+ // TODO: Figure out how we can do this without cloning
+ // TODO: Verify if this actually makes things faster or not...
+ #[derive(Clone)]
+pub struct Converter {
+    pow_rgb_to_xyz: Vec<f32>,
+    pow_xyz_to_lab: Vec<f32>,
+    pow_lab_to_xyz: Vec<f32>,
+    pow_xyz_to_rgb: Vec<f32>
+}
+
+pub fn create_converter() -> Converter {
+    Converter {
+        pow_rgb_to_xyz: precompute_powf(2.4),
+        pow_xyz_to_lab: precompute_powf(1.0 / 3.0),
+        pow_lab_to_xyz: precompute_powf(3.0),
+        pow_xyz_to_rgb: precompute_powf(1.0 / 2.4)
+    }
+}
+
+fn precompute_powf(exponent: f32) -> Vec<f32> {
+    (0..10000)
+        .map(|x: i32| (x as f32) * 0.01)
+        .map(|x: f32| x.powf(exponent))
+        .collect()
+}
+
+fn cached_powf(value: f32, pow: &Vec<f32>) -> f32 {
+    let scaled_value: f32 = value * 100.0;
+    let index = scaled_value as usize;
+    let alpha: f32 = scaled_value - (index as f32);
+    if index < pow.len() - 1 {
+        alpha * pow[index + 1] + (1.0 - alpha) * pow[index]
+    } else {
+        // TODO: This shouldn't happen, so better think about what's best here
+        value
+    }
+}
+
 const REFERENCE_X: f32 = 95.047;
 const REFERENCE_Y: f32 = 100.0;
 const REFERENCE_Z: f32 = 108.883;
 
-pub fn rgb_image_to_lab(rgb_image: &RgbImage) -> LabImage {
-    let lab_pixels: Vec<LabPixel> = rgb_image.pixels.par_iter()
-        .map(|rgb_pixel: &RgbPixel| {
-            let xyz_pixel: XyzPixel = rgb_pixel_to_xyz(&rgb_pixel);
-            let lab_pixel: LabPixel = xyz_pixel_to_lab(&xyz_pixel);
-            lab_pixel
-        })
-        .collect();
+impl Converter {
+    pub fn rgb_image_to_lab(&self, rgb_image: &RgbImage) -> LabImage {
+        let lab_pixels: Vec<LabPixel> = rgb_image.pixels.par_iter()
+            .map(|rgb_pixel: &RgbPixel| {
+                let xyz_pixel: XyzPixel = self.rgb_pixel_to_xyz(&rgb_pixel);
+                let lab_pixel: LabPixel = self.xyz_pixel_to_lab(&xyz_pixel);
+                lab_pixel
+            })
+            .collect();
 
-    LabImage {
-        width: rgb_image.width,
-        height: rgb_image.height,
-        pixels: lab_pixels
+        LabImage {
+            width: rgb_image.width,
+            height: rgb_image.height,
+            pixels: lab_pixels
+        }
     }
-}
 
-pub fn lab_image_to_rgb(lab_image: &LabImage) -> RgbImage {
-    let rgb_pixels: Vec<RgbPixel> = lab_image.pixels.par_iter()
-        .map(|lab_pixel: &LabPixel| {
-            let xyz_pixel: XyzPixel = lab_pixel_to_xyz(&lab_pixel);
-            let rgb_pixel: RgbPixel = xyz_pixel_to_rgb(&xyz_pixel);
-            rgb_pixel
-        })
-        .collect();
+    pub fn lab_image_to_rgb(&self, lab_image: &LabImage) -> RgbImage {
+        let rgb_pixels: Vec<RgbPixel> = lab_image.pixels.par_iter()
+            .map(|lab_pixel: &LabPixel| {
+                if lab_pixel.lightness < 1.0 {
+                    RgbPixel {
+                        red: 0.0,
+                        green: 0.0,
+                        blue: 0.0
+                    }
+                } else {
+                    let xyz_pixel: XyzPixel = self.lab_pixel_to_xyz(&lab_pixel);
+                    let rgb_pixel: RgbPixel = self.xyz_pixel_to_rgb(&xyz_pixel);
+                    rgb_pixel
+                }
+            })
+            .collect();
 
-    RgbImage {
-        width: lab_image.width,
-        height: lab_image.height,
-        pixels: rgb_pixels
+        RgbImage {
+            width: lab_image.width,
+            height: lab_image.height,
+            pixels: rgb_pixels
+        }
     }
-}
 
-fn rgb_pixel_to_xyz(rgb_pixel: &RgbPixel) -> XyzPixel {
-    fn scale(value: f32) -> f32 {
+
+    fn scale_rgb_to_xyz(&self, value: f32) -> f32 {
         if value > 0.04045 {
-            100.0 * ((value + 0.055) / 1.055).powf(2.4)
+            100.0 * cached_powf((value + 0.055) / 1.055, &self.pow_rgb_to_xyz)
         } else {
             100.0 * value / 12.92
         }
     }
 
-    let scaled_red: f32 = scale(rgb_pixel.red);
-    let scaled_green: f32 = scale(rgb_pixel.green);
-    let scaled_blue: f32 = scale(rgb_pixel.blue);
+    fn rgb_pixel_to_xyz(&self, rgb_pixel: &RgbPixel) -> XyzPixel {
+        let scaled_red: f32 = self.scale_rgb_to_xyz(rgb_pixel.red);
+        let scaled_green: f32 = self.scale_rgb_to_xyz(rgb_pixel.green);
+        let scaled_blue: f32 = self.scale_rgb_to_xyz(rgb_pixel.blue);
 
-    XyzPixel {
-        x: scaled_red * 0.4124 + scaled_green * 0.3576 + scaled_blue * 0.1805,
-        y: scaled_red * 0.2126 + scaled_green * 0.7152 + scaled_blue * 0.0822,
-        z: scaled_red * 0.0193 + scaled_green * 0.1192 + scaled_blue * 0.9505
+        XyzPixel {
+            x: scaled_red * 0.4124 + scaled_green * 0.3576 + scaled_blue * 0.1805,
+            y: scaled_red * 0.2126 + scaled_green * 0.7152 + scaled_blue * 0.0822,
+            z: scaled_red * 0.0193 + scaled_green * 0.1192 + scaled_blue * 0.9505
+        }
     }
-}
 
-fn xyz_pixel_to_lab(xyz_pixel: &XyzPixel) -> LabPixel {
-    fn scale(value: f32) -> f32 {
+
+    fn scale_xyz_to_lab(&self, value: f32) -> f32 {
         if value > 0.008856 {
-            value.powf(1.0 / 3.0)
+            cached_powf(value, &self.pow_xyz_to_lab)
         } else {
             (7.787 * value) + (16.0 / 116.0)
         }
     }
 
-    let var_x: f32 = scale(xyz_pixel.x / REFERENCE_X);
-    let var_y: f32 = scale(xyz_pixel.y / REFERENCE_Y);
-    let var_z: f32 = scale(xyz_pixel.z / REFERENCE_Z);
+    fn xyz_pixel_to_lab(&self, xyz_pixel: &XyzPixel) -> LabPixel {
+        let var_x: f32 = self.scale_xyz_to_lab(xyz_pixel.x / REFERENCE_X);
+        let var_y: f32 = self.scale_xyz_to_lab(xyz_pixel.y / REFERENCE_Y);
+        let var_z: f32 = self.scale_xyz_to_lab(xyz_pixel.z / REFERENCE_Z);
 
-    LabPixel {
-        lightness: (116.0 * var_y) - 16.0,
-        tint: 500.0 * (var_x - var_y),
-        temperature: 200.0 * (var_y - var_z)
+        LabPixel {
+            lightness: (116.0 * var_y) - 16.0,
+            tint: 500.0 * (var_x - var_y),
+            temperature: 200.0 * (var_y - var_z)
+        }
     }
-}
 
-fn lab_pixel_to_xyz(lab_pixel: &LabPixel) -> XyzPixel {
-    fn scale(value: f32) -> f32 {
+
+    fn scale_lab_to_xyz(&self, value: f32) -> f32 {
         if value > 0.008856 {
-            value.powf(3.0)
+            cached_powf(value, &self.pow_lab_to_xyz)
         } else {
             (value - 16.0 / 116.0) / 7.787
         }
     }
 
-    let var_y: f32 = (lab_pixel.lightness + 16.0) / 116.0;
-    let var_x: f32 = (lab_pixel.tint / 500.0) + var_y;
-    let var_z: f32 = var_y - (lab_pixel.temperature / 200.0);
+    fn lab_pixel_to_xyz(&self, lab_pixel: &LabPixel) -> XyzPixel {
+        let var_y: f32 = (lab_pixel.lightness + 16.0) / 116.0;
+        let var_x: f32 = (lab_pixel.tint / 500.0) + var_y;
+        let var_z: f32 = var_y - (lab_pixel.temperature / 200.0);
 
-    XyzPixel {
-        x: scale(var_x) * REFERENCE_X,
-        y: scale(var_y) * REFERENCE_Y,
-        z: scale(var_z) * REFERENCE_Z
+        XyzPixel {
+            x: self.scale_lab_to_xyz(var_x) * REFERENCE_X,
+            y: self.scale_lab_to_xyz(var_y) * REFERENCE_Y,
+            z: self.scale_lab_to_xyz(var_z) * REFERENCE_Z
+        }
     }
-}
 
-fn xyz_pixel_to_rgb(xyz_pixel: &XyzPixel) -> RgbPixel {
-    fn scale(value: f32) -> f32 {
+
+    fn scale_xyz_to_rgb(&self, value: f32) -> f32 {
         let scaled_value: f32 = value / 100.0;
         if scaled_value > 0.0031308 {
-            1.055 * scaled_value.powf(1.0 / 2.4) - 0.055
+            1.055 * cached_powf(scaled_value, &self.pow_xyz_to_rgb) - 0.055
         } else {
             scaled_value * 12.92
         }
     }
 
-    RgbPixel {
-        red:   scale( xyz_pixel.x * 3.2406 - xyz_pixel.y * 1.5372 - xyz_pixel.z * 0.4986),
-        green: scale(-xyz_pixel.x * 0.9689 + xyz_pixel.y * 1.8758 + xyz_pixel.z * 0.0415),
-        blue:  scale( xyz_pixel.x * 0.0557 - xyz_pixel.y * 0.2040 + xyz_pixel.z * 1.0570)
+    fn xyz_pixel_to_rgb(&self, xyz_pixel: &XyzPixel) -> RgbPixel {
+        RgbPixel {
+            red:   self.scale_xyz_to_rgb( xyz_pixel.x * 3.2406 - xyz_pixel.y * 1.5372 - xyz_pixel.z * 0.4986),
+            green: self.scale_xyz_to_rgb(-xyz_pixel.x * 0.9689 + xyz_pixel.y * 1.8758 + xyz_pixel.z * 0.0415),
+            blue:  self.scale_xyz_to_rgb( xyz_pixel.x * 0.0557 - xyz_pixel.y * 0.2040 + xyz_pixel.z * 1.0570)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,24 @@ mod functions;
 
 use crate::types::*;
 
+use conversions::{Converter, create_converter};
 use iced;
 use image;
 use num;
 
 pub fn main() -> iced::Result {
-    iced::application("A cool image editor", update, view)
+    iced::application("A cool image editor", Main::update, Main::view)
         .theme(|_| iced::Theme::Dark)
         .resizable(true)
-        .run_with(initialize)
+        .run()
 }
-
-fn initialize() -> (State, iced::Task<Message>) {
-    let source_image: LabImage = load_image_as_lab();
-    let parameters: Parameters = Parameters::default();
-
-    let handle = update_image(source_image.clone(), parameters);
-
-    let state: State = State {
-        source_image: source_image,
-        parameters: Parameters::default(),
-        handle: handle
-    };
-    (state, iced::Task::none())
-}
-
+    
 #[derive(Default, Clone, Copy)]
 struct Parameters {
     brightness: f32,
     contrast: f32,
     tint: f32,
     temperature: f32
-}
-
-struct State {
-    source_image: LabImage,
-    parameters: Parameters,
-    handle: iced::widget::image::Handle
 }
 
 #[derive(Debug, Clone)]
@@ -52,69 +33,29 @@ enum Message {
     ImageUpdated(iced::widget::image::Handle) // TODO: Maybe don't return a handle?
 }
 
-fn update(state: &mut State, message: Message) -> iced::Task<Message> {
-    match message {
-        Message::BrightnessChanged(brightness) => {
-            state.parameters.brightness = brightness;
-            update_image_task(&state)
-        },
-        Message::ContrastChanged(contrast) => {
-            state.parameters.contrast = contrast;
-            update_image_task(&state)
-        },
-        Message::TintChanged(tint) => {
-            state.parameters.tint = tint;
-            update_image_task(&state)
-        },
-        Message::TemperatureChanged(temperature) => {
-            state.parameters.temperature = temperature;
-            update_image_task(&state)
-        },
-        Message::ImageUpdated(handle) => {
-            state.handle = handle;
-            iced::Task::none()
-        }
-    }
+struct Main {
+    converter: Converter,
+    source_image: LabImage,
+    parameters: Parameters,
+    handle: iced::widget::image::Handle
 }
-
-fn update_image(mut image: LabImage, parameters: Parameters) -> iced::widget::image::Handle {
+    
+fn update_image(converter: &Converter, mut image: LabImage, parameters: Parameters) -> iced::widget::image::Handle {
     functions::brightness(&mut image, parameters.brightness);
     functions::contrast(&mut image, parameters.contrast);
     functions::tint(&mut image, parameters.tint);
     functions::temperature(&mut image, parameters.temperature);
-    let rgb_image: RgbImage = conversions::lab_image_to_rgb(&image);
+    let rgb_image: RgbImage = converter.lab_image_to_rgb(&image);
     iced::widget::image::Handle::from_rgba(
         rgb_image.width as u32,
         rgb_image.height as u32,
         rgb_image_to_bytes(&rgb_image))
 }
 
-async fn update_image_async(image: LabImage, parameters: Parameters) -> iced::widget::image::Handle {
-    update_image(image, parameters)
+async fn update_image_async(converter: Converter, image: LabImage, parameters: Parameters) -> iced::widget::image::Handle {
+    update_image(&converter, image, parameters)
 }
-
-fn update_image_task(state: &State) -> iced::Task<Message> {
-    iced::Task::perform(update_image_async(state.source_image.clone(), state.parameters), Message::ImageUpdated)
-}
-
-fn view(state: &State) -> iced::Element<Message> {
-    iced::widget::row![
-            iced::widget::image(state.handle.clone()),
-            view_sliders(&state)
-        ]
-        .into()
-}
-
-fn view_sliders(state: &State) -> iced::Element<Message> {
-    iced::widget::column![
-            iced::widget::slider(-100.0..=100.0, state.parameters.brightness, Message::BrightnessChanged),
-            iced::widget::slider(-100.0..=100.0, state.parameters.contrast, Message::ContrastChanged),
-            iced::widget::slider(-100.0..=100.0, state.parameters.tint, Message::TintChanged),
-            iced::widget::slider(-100.0..=100.0, state.parameters.temperature, Message::TemperatureChanged)
-        ]
-        .into()
-}
-
+    
 fn load_image() -> RgbImage {
     let source_image = image::open("example.png").unwrap().into_rgb32f();
     let width: u32 = source_image.width();
@@ -138,9 +79,9 @@ fn load_image() -> RgbImage {
     }
 }
 
-fn load_image_as_lab() -> LabImage {
+fn load_image_as_lab(converter: &Converter) -> LabImage {
     let image: RgbImage = load_image();
-    conversions::rgb_image_to_lab(&image)
+    converter.rgb_image_to_lab(&image)
 }
 
 fn pixel_value_to_u8(value: f32) -> u8 {
@@ -158,4 +99,75 @@ fn rgb_image_to_bytes(image: &RgbImage) -> iced::advanced::image::Bytes {
     }
 
     iced::advanced::image::Bytes::from(buffer)
+}
+
+impl Main {
+
+    fn new() -> Self {
+        let converter: Converter = create_converter();
+        let source_image: LabImage = load_image_as_lab(&converter);
+        let parameters: Parameters = Parameters::default();
+    
+        let handle = update_image(&converter, source_image.clone(), parameters);
+
+        Self {
+            converter,
+            source_image,
+            parameters,
+            handle
+        }
+    }
+    
+    fn update(&mut self, message: Message) -> iced::Task<Message> {
+        match message {
+            Message::BrightnessChanged(brightness) => {
+                self.parameters.brightness = brightness;
+                self.update_image_task()
+            },
+            Message::ContrastChanged(contrast) => {
+                self.parameters.contrast = contrast;
+                self.update_image_task()
+            },
+            Message::TintChanged(tint) => {
+                self.parameters.tint = tint;
+                self.update_image_task()
+            },
+            Message::TemperatureChanged(temperature) => {
+                self.parameters.temperature = temperature;
+                self.update_image_task()
+            },
+            Message::ImageUpdated(handle) => {
+                self.handle = handle;
+                iced::Task::none()
+            }
+        }
+    }
+    
+    fn update_image_task(&self) -> iced::Task<Message> {
+        iced::Task::perform(update_image_async(self.converter.clone(), self.source_image.clone(), self.parameters), Message::ImageUpdated)
+    }
+    
+    fn view(&self) -> iced::Element<Message> {
+        iced::widget::row![
+                iced::widget::image(self.handle.clone()),
+                self.view_sliders()
+            ]
+            .into()
+    }
+    
+    fn view_sliders(&self) -> iced::Element<Message> {
+        iced::widget::column![
+                iced::widget::slider(-100.0..=100.0, self.parameters.brightness, Message::BrightnessChanged),
+                iced::widget::slider(-100.0..=100.0, self.parameters.contrast, Message::ContrastChanged),
+                iced::widget::slider(-100.0..=100.0, self.parameters.tint, Message::TintChanged),
+                iced::widget::slider(-100.0..=100.0, self.parameters.temperature, Message::TemperatureChanged)
+            ]
+            .into()
+    }
+}
+
+impl Default for Main {
+    fn default() -> Self {
+        Self::new()
+    }
 }
