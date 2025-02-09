ce325fcb3f17b47163473bd53c9b55e9092262c6
oscfri <oscar.c.friberg@gmail.com>
2025-02-09 18:29:53 +0100

    More renaming

--- src/pipeline/pipeline.rs
+++ src/pipeline/pipeline.rs
@@ -8,7 +8,7 @@ use super::{crop_uniform, parameter_uniform, radial_parameter};
 
 pub struct Pipeline {
     pipeline: wgpu::RenderPipeline,
-    vertices: wgpu::Buffer,
+    vertex_buffer: wgpu::Buffer,
     camera_buffer: wgpu::Buffer,
     parameter_buffer: wgpu::Buffer,
     crop_buffer: wgpu::Buffer,
@@ -21,7 +21,7 @@ pub struct Pipeline {
 impl Pipeline {
     pub fn new(
             pipeline: wgpu::RenderPipeline,
-            vertices: wgpu::Buffer,
+            vertex_buffer: wgpu::Buffer,
             camera_buffer: wgpu::Buffer,
             parameter_buffer: wgpu::Buffer,
             crop_buffer: wgpu::Buffer,
@@ -31,7 +31,7 @@ impl Pipeline {
             diffuse_bind_group: wgpu::BindGroup) -> Self {
         Self {
             pipeline,
-            vertices,
+            vertex_buffer,
             camera_buffer,
             parameter_buffer,
             crop_buffer,
@@ -49,7 +49,7 @@ impl Pipeline {
             camera_uniform: &camera_uniform::CameraUniform,
             parameter_uniform: &parameter_uniform::ParameterUniform,
             crop_uniform: &crop_uniform::CropUniform,
-            radial_parameter: &radial_parameter::RadialParameters) {
+            radial_parameters: &radial_parameter::RadialParameters) {
         let texture_size = wgpu::Extent3d {
             width: image.width as u32,
             height: image.height as u32,
@@ -58,7 +58,7 @@ impl Pipeline {
         queue.write_buffer(&self.camera_buffer, 0, bytemuck::bytes_of(camera_uniform));
         queue.write_buffer(&self.parameter_buffer, 0, bytemuck::bytes_of(parameter_uniform));
         queue.write_buffer(&self.crop_buffer, 0, bytemuck::bytes_of(crop_uniform));
-        queue.write_buffer(&self.radial_parameters_buffer, 0, bytemuck::bytes_of(radial_parameter));
+        queue.write_buffer(&self.radial_parameters_buffer, 0, bytemuck::bytes_of(radial_parameters));
         queue.write_texture(
             wgpu::ImageCopyTexture {
                 texture: &self.diffuse_texture,
@@ -101,7 +101,7 @@ impl Pipeline {
         pass.set_pipeline(&self.pipeline);
         pass.set_bind_group(0, &self.uniform_bind_group, &[]);
         pass.set_bind_group(1, &self.diffuse_bind_group, &[]);
-        pass.set_vertex_buffer(0, self.vertices.slice(..));
+        pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         pass.draw(0..6, 0..1);
     }
 }
